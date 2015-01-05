a4e8cee4717fa28320324650504348f6d6e0dac3
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-01-05 13:20:43 -0500

    successful grayscale conversion with sample add, <insert joke here>

--- ImageProcessing.java
+++ ImageProcessing.java
@@ -5,24 +5,37 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 public class ImageProcessing{
 
-    public int[][] convertToGrayscale(String imagelocation){ // converts image at imagelocation to a 2d array of it's grayscale
-	BufferedImage image;
-	rawimage = new File(imagelocation);
-	image = ImageIO.read(rawimage);
+    public static int[][] convertToGrayscale(String imagelocation) throws IOException{ // converts image at imagelocation to a 2d array of it's grayscale
+	File rawimage = new File(imagelocation);
+	BufferedImage image = ImageIO.read(rawimage);
 	int height = image.getHeight();
 	int width = image.getWidth();
-	int[][] ans = new int[width][height]
+	int[][] ans = new int[width][height];
 	for( int x = 0;x<width;x++){
 	    for(int y = 0; y<height;y++){
 		Color c = new Color(image.getRGB(x,y));
 		int red = (int)(c.getRed() * 0.299);
 		int green = (int)(c.getGreen() * 0.587);
 		int blue = (int)(c.getBlue() *0.114);
-		ans[x][y] = red + green + blue
+		ans[x][y] = red + green + blue;
 	    }
 	}
 	return ans;
     }
 	
+    public static void grayscaletoimage(String destination, int[][] input) throws IOException{
+	BufferedImage image = new BufferedImage(input.length,input[0].length,1);
+	for(int x = 0;x<input.length;x++){
+	    for(int y=0;y<input[x].length;y++){
+		Color c  = new Color(input[x][y],input[x][y],input[x][y]);
+		image.setRGB(x,y,c.getRGB());
+	    }
+	}
+	File dest = new File(destination);
+	ImageIO.write(image,"jpg",dest);
+    }
 
+    public static void main(String[] args) throws IOException{
+	grayscaletoimage("grascale.jpg", convertToGrayscale("Fate.zero.full.1072802.jpg"));
+    }
 }
