7f97918b710e5b18b05c328f33dead6d2514139a
Jion Fairchild <jijiglobe@yahoo.com>
2015-01-12 13:20:01 -0500

    Write some shtuff that converts rgb to hex

--- test/Colors.java
+++ test/Colors.java
@@ -1,5 +1,15 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 public class Colors{
     private String hex = "0123456789ABCDEF";
+    
+    public String hexCode(Color thiscolor){
+	return "#"+intToHex(thiscolor.getRed())+intToHex(thiscolor.getGreen())+intToHex(thiscolor.getBlue());
+    }
+
     public static String intToHex(int val){
 	return hex[val/16] + hex[val%16];
     }
