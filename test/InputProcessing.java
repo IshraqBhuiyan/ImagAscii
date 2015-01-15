e101075f3574bb9ee45684d24a9c62e21fb330a3
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-01-15 13:17:18 -0500

    finish universal format reader and start gif Writer

--- test/InputProcessing.java
+++ test/InputProcessing.java
@@ -4,15 +4,27 @@ import java.net.URL;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
 import java.util.ArrayList;
-public final class VideoProcessing{
+public final class InputProcessing{
 
-    public static ArrayList<BufferedImage> getImage(String url) throws Exception{
-	ArrayList<BufferedImage> inStream = new ArrayList<BufferedImage>;
+    public static ArrayList<BufferedImage> getImage(String url) throws IOException{
+	ArrayList<BufferedImage> inStream = new ArrayList<BufferedImage>; //if number of elements in inStream > 1, then it is a gif, elsewise it is a single image
 	ImageReader image = ImageIO.read(new URL(url));
+	try{
+	    int i = image.getMinIndex();
+	    while(true){
+		inStream.add(image.read(i));
+		i++;
+	    }
+	}catch(IndexOutOfBoundsException e){
+	    //does nothing, just breaks off reading the image
+	}
+	return inStream;
 
     }
 	
-					      
+    public static void writeGif(String output, ArrayList<BufferedImage> gifSequence){
+
+    }
 
 
 
