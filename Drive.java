4cb7f78c95c29c799c8f08846224cd16099ef981
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-01-20 23:21:19 -0500

    fix some more dumb merge errors

--- Drive.java
+++ Drive.java
@@ -85,17 +85,17 @@ public class  Drive{
 	//System.out.println("begin rendering");
 	//BufferedImage renderIMG = ImageOutput.renderTextToImage(splitText);
 	//ImageIO.write(renderIMG, "png", new File(outimg + ".png"));
-	String[] splitText = ImageProcessing.imageToAsciiArray(input, 3);;
-	System.out.println("begin rendering");
-	BufferedImage renderIMG = ImageOutput.renderTextToImage(splitText);
-	ImageIO.write(renderIMG, "png", new File(outimg + ".png"));
+	//String[] splitText = ImageProcessing.imageToAsciiArray(input, 3);;
+	//System.out.println("begin rendering");
+	//BufferedImage renderIMG = ImageOutput.renderTextToImage(splitText);
+	//ImageIO.write(renderIMG, "png", new File(outimg + ".png"));
 	if(!(outtxt.equals("noout"))){
 	    PrintWriter txtfile = new PrintWriter(outtxt+".txt");
 	    txtfile.println(text);
 	    txtfile.close();
 	}
 
-	System.out.println("begin jframing");
+	//System.out.println("begin jframing");
 	SwagWindow output = new SwagWindow(input,text,Iwidth,Iheight,2);
 	output.pack();
 	output.setVisible(true);
